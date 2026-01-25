c3bb1c8a71dcfebd4ea44000361d6ed3685c6842
Jason-Ch02 <25jzchen1@stu.edu.cn>
2026-01-25 21:41:51 +0800

    Point-circle relationship

---
+++ 圆和点之间的关系/圆类.cpp
@@ -0,0 +1,19 @@
+#include"‘≤¿‡.h"
+#include"µ„¿‡.h"
+
+	void Circle::setR(int R)
+	{
+		r = R;
+	}
+	int Circle::getR()
+	{
+		return r;
+	}
+	void Circle::setC(Point C)
+	{
+		center = C;
+	}
+	Point Circle::getC()
+	{
+		return center;
+	}
